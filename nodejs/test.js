2b0a27e403a11479a6afaecc80a8f775339b6a1d
oBlank <dyh1919@gmail.cm>
2021-02-03 15:33:31 +0800

    more test keywords

--- nodejs/test.js
+++ nodejs/test.js
@@ -8,5 +8,5 @@ function test(w) {
     // console.log(w, SensitiveWordsTrie.sensitiveCheck(w));
 }
 
-test("狗狗的狗粉丝在赌博场所说黑话，骂习大习近平骂习近平骂习近平骂习近平骂习近平");
+test("狗狗的狗粉丝在赌博场所说黑话，骂习大习习近平骂习近平骂习近平骂习近平骂习近平");
 test("这是一段正常的文字");
