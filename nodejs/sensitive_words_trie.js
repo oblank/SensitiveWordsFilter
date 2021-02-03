f96a630b59a993e18cc0cda8ea01acb2ebf8d4a6
oBlank <dyh1919@gmail.cm>
2021-02-03 14:52:08 +0800

    update

--- nodejs/sensitive_words_trie.js
+++ nodejs/sensitive_words_trie.js
@@ -14,6 +14,7 @@ module.exports = {
         }
     },
 
+    // 添加敏感词，构建Trie
     addWord(word) {
         if (word != null || word !== "") {
             let parent = this.tree;
@@ -27,27 +28,33 @@ module.exports = {
         }
     },
 
+    // 移除空格
+    removeSpace(text) {
+        return text.toString().trim().replace(/[\s| ]/ig, '');
+    },
+
     // 检查是否包含敏感词，检查到一个则停止检测
     sensitiveCheck(word) {
-        var parent = this.tree,
+        word = this.removeSpace(word);
+        if (word === "" || word === null) {
+            return word;
+        }
+
+        let parent = this.tree,
             exists = false;
 
-        for (var i = 0; i < word.length; i++) {
+        for (let i = 0; i < word.length; i++) {
             // TODO 加强干扰字符的处理
             if (['*', '', ' ', ',', '.', '?'].indexOf(word[ i ]) !== -1) {
                 // 过滤掉干扰字符
                 continue;
             }
 
-            for (var j = i; j < word.length; j++) {
-                console.log(`i=${i}, j=${j}, word[${j}]=${word[ j ]}, parent[word[j]] = ${parent[ word[ j ] ]}`)
+            for (let j = i; j < word.length; j++) {
                 if (!parent[ word[ j ] ]) {
                     exists = false;
-                    parent = parent;
                     break;
-                }
-
-                if (parent[ word[ j ] ].isEnd) {
+                } else if (parent[ word[ j ] ] && parent[ word[ j ] ].isEnd) {
                     exists = true;
                     break;
                 }
@@ -62,62 +69,46 @@ module.exports = {
     },
 
     // 替换敏感词
-    filter(word, options) {
-        let parent = this.tree;
-        for (let i = 0; i < word.length; i++) {
-            if (['*', '', ' ', ',', '.', '?'].indexOf(word[ i ]) !== -1) {
-                // 过滤掉干扰字符
-                continue;
-            }
-            let exists        = false,
-                skip          = 0,
-                sensitiveWord = '';
-
-            for (let j = i; j < word.length; j++) {
-                sensitiveWord = sensitiveWord + word[ j ];
-                skip = j - i;
-                if (['*', '', ' ', ',', '.', '?'].indexOf(word[ j ]) !== -1) {
-                    // 过滤掉干扰字符
-                    if (j === word.length - 1 && skip !== 0) {
-                        exists = true;
-                    }
-                    continue;
-                }
-
-                if (!parent[ word[ j ] ]) {
-                    exists = false;
-                    // parent = parent;
-                    break;
-                }
-
-                // 最大长度匹配
-                if (parent[ word[ j ] ].isEnd && (Object.keys(parent[ word[ j ] ]).length === 1 || j === word.length - 1)) {
-                    exists = true;
-                    break;
-                }
-                parent = parent[ word[ j ] ];
-            }
-
-            if (skip > 1) {
-                i += skip - 1;
-            }
-
-            if (!exists) {
-                continue;
-            }
-
-            let stars = '*';
-            for (var k = 0; k < skip; k++) {
-                stars = stars + '*';
-            }
-
-            word = word.replace(new RegExp(sensitiveWord, 'g'), stars);
+    replace(text, options) {
+        text = this.removeSpace(text);
+        if (text === "" || text === null) {
+            return text;
         }
+
+        let sensitiveKeyWords = [];
+        let parent = this.tree;
+        for (let i = 0; i < text.length; i++) {
+            if(parent[text[i]] === null  || parent[text[i]] === undefined) {
+                continue
+            }
+
+            let tmpWords = [];
+
+            for (let j = i; j < text.length; j++) {
+                console.log(`i=${i}, j=${j}`)
+                // 找到了就暂存，并将parent指向其子节点
+                if (parent[ text[ j ] ] !== undefined) {
+                    // console.log("word[ j ]", word[ j ]);
+                    tmpWords.push(text[ j ]);
+                    parent = parent[ text[ j ] ];
+                }
+
+                if (parent[text[j]] && parent[text[j]].isEnd) {
+                    console.log(tmpWords);
+                    sensitiveKeyWords.push(tmpWords.join());
+                    break
+                }
+            }
+        }
+
+        console.log(sensitiveKeyWords);
+        // word = word.replace(new RegExp(sensitiveWord, 'g'), stars);
+
 
         if (options && options.callback) {
             options.callback(word);
         } else {
-            return word;
+            return text;
         }
     }
 };
--- nodejs/test.js
+++ nodejs/test.js
@@ -2,18 +2,11 @@ let SensitiveWordsTrie = require("./sensitive_words_trie");
 
 
 SensitiveWordsTrie.init();
-SensitiveWordsTrie.loadFile("./keywords");
 
 function test(w) {
-    // console.log(test.sensitiveCheck(w), w, test.filter(w));
-    console.log(w, SensitiveWordsTrie.sensitiveCheck(w));
+    console.log(SensitiveWordsTrie.sensitiveCheck(w), SensitiveWordsTrie.replace(w));
+    // console.log(w, SensitiveWordsTrie.sensitiveCheck(w));
 }
 
-test("这是一个习 近平大大的笑话，习近平,,习近平,,");
-// test("习近平大大");
-// test("习近平");
-// test("习近平前妻");
-// test("习大大");
-// test("习大大 ");
-// test("习大大前妻");
-// test("小屌丝");
+test("狗狗的狗粉丝在赌博场所说黑话，骂习大习近平骂习近平骂习近平骂习近平骂习近平");
+test("这是一段正常的文字");
